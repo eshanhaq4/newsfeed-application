91e8519e1161eaf8c4921390492e14596f49accb
eshanhaq4 <59237231+eshanhaq4@users.noreply.github.com>
2026-02-06 23:59:44 -0600

    Implement Post and Like models and testing initial migrations, also learning more Django and testing.

--- backend/feed/models.py
+++ backend/feed/models.py
@@ -3,4 +3,23 @@ from django.db import models
 # Create your models here.
 
 # posts
-#TODO 
+class Post(models.Model):
+    creator_id = models.IntegerField()
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    edited_at = models.DateTimeField(null=True, blank=True)
+    like_count = models.IntegerField(default=0)
+
+    def __str__(self):\
+        return f"Post {self.id} by User {self.creator_id}"
+    
+class Like(models.Model):
+    user_id = models.IntegerField()
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Like by User {self.user_id} on Post {self.post.id}"
+    
+    class Meta:
+        unique_together = ('user_id', 'post')
