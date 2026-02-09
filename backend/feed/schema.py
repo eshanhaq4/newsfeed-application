dd3734c276578de21fbd2d551337c3981637a61d
eshanhaq4 <59237231+eshanhaq4@users.noreply.github.com>
2026-02-08 20:25:46 -0600

    Added and implemented more of the needed queries and mutations (create, edit, delete, like/unlike), with a working state

--- backend/feed/schema.py
+++ backend/feed/schema.py
@@ -12,19 +12,61 @@ class PostGQL:
     edited_at: typing.Optional[datetime]
     like_count: int
 
+def post_gql(post: models.Post) -> PostGQL:
+    return PostGQL(
+        id=post.id,
+        creator_id=post.creator_id,
+        content=post.content,
+        created_at=post.created_at,
+        edited_at=post.edited_at,
+        like_count=post.like_count,
+    )
 
 @strawberry.type
 class Query:
     @strawberry.field
     def posts(self) -> typing.List[PostGQL]:
         posts = models.Post.objects.all().order_by("-created_at")
-        return [PostGQL(
-            id=post.id,
-            creator_id=post.creator_id,
-            content=post.content,
-            created_at=post.created_at,
-            edited_at=post.edited_at if post.edited_at else None,
-            like_count=post.likes.count()
-        ) for post in posts]
+        return [post_gql(post) for post in posts]
     
-schema = strawberry.Schema(query=Query)
+@strawberry.type
+class Mutation:
+    @strawberry.mutation
+    def create_post(self, creator_id: int, content: str) -> PostGQL:
+        post = models.Post.objects.create(creator_id=creator_id, content=content)
+        return post_gql(post)
+
+    @strawberry.mutation
+    def toggle_like(self, user_id: int, post_id: int) -> PostGQL:
+        post = models.Post.objects.get(id=post_id)
+        created = models.Like.objects.filter(user_id=user_id, post=post)
+
+        if created.exists():
+            created.delete()
+            post.like_count -= 1
+        else:
+            models.Like.objects.create(user_id=user_id, post=post)
+            post.like_count += 1
+
+        post.save()
+        post.refresh_from_db()
+
+        return post_gql(post)
+    
+    @strawberry.mutation
+    def edit_post(self, post_id: int, new_content: str) -> PostGQL:
+        post = models.Post.objects.get(id=post_id)
+        post.content = new_content
+        post.edited_at = datetime.now()
+        post.save()
+        post.refresh_from_db()
+
+        return post_gql(post)
+    
+    @strawberry.mutation
+    def delete_post(self, post_id: int) -> int:
+        post = models.Post.objects.get(id=post_id)
+        post.delete()
+        return post_id
+
+schema = strawberry.Schema(query=Query, mutation=Mutation)
