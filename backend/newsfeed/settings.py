99e0252a6fc9ee56d997be002e9fb09de9d5fb46
eshanhaq4 <59237231+eshanhaq4@users.noreply.github.com>
2026-02-12 17:22:40 -0600

    Set up frontend part of the project and implemented initial feed integration into page.tsx to verify and test that the backend fully works and connected to the frontend (including pagination), fixing some bugs and preparing to do abstraction and further features next.

--- backend/newsfeed/settings.py
+++ backend/newsfeed/settings.py
@@ -38,9 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'feed',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -111,6 +113,8 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 
 # Static files (CSS, JavaScript, Images)
